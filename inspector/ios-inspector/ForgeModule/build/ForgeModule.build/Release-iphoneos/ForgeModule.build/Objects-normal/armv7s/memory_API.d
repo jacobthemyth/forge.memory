f919fa25631c6d2dabfd64edb786abf70a0f8e82
Jake Smith <jake@mustconverge.com>
2014-02-10 13:07:02 -0500

    Add ios binary

---
+++ inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphoneos/ForgeModule.build/Objects-normal/armv7s/memory_API.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/jake/Code/modules/forge.memory/inspector/ios-inspector/ForgeModule/memory_API.m \
+  /Users/jake/Code/modules/forge.memory/inspector/ios-inspector/ForgeModule/memory_API.h
---
+++ inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphonesimulator/ForgeModule.build/Objects-normal/i386/memory_EventListener.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/jake/Code/modules/forge.memory/inspector/ios-inspector/ForgeModule/memory_EventListener.m \
+  /Users/jake/Code/modules/forge.memory/inspector/ios-inspector/ForgeModule/memory_EventListener.h
